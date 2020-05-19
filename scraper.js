4a4a311234f7a5362f2c0a589223f9feb07f4b49
Crystal Danger <cdneal091@gmail.com>
2020-05-18 22:33:24 -0400

    works

---
+++ scraper.js
@@ -0,0 +1,40 @@
+require('dotenv').config()
+const fs = require('fs').promises
+
+const {Builder, By, Key, until} = require('selenium-webdriver');
+
+(async function example() {
+  let accInfo = {}
+  let driver = await new Builder().forBrowser('chrome').build();
+  try {
+    await driver.get('https://www.nelnet.com');
+    const username = await driver.wait(until.elementLocated(By.id('username')))
+    await driver.wait(until.elementIsEnabled(username), 60000);
+    await username.sendKeys(process.env.NELNET_USER, Key.RETURN).catch(console.log);
+    const password = await driver.wait(until.elementLocated(By.id('Password')), 30000, 'Timed out on password', 3000);
+    await password.sendKeys(process.env.NELNET_PASS, Key.RETURN);
+    await driver.wait(until.elementLocated(By.css('a[href="/Loan/Details"]')));
+    await driver.get('https://www.nelnet.com/Loan/Details');
+    await driver.wait(until.elementLocated(By.css('.feature.account-row')));
+    const accDetails = await driver.findElement(By.css('.feature.account-row'));
+    const detailList = await accDetails.findElements(By.css('.ng-binding.text-xs-right'));
+    accInfo.number = await detailList[0].getText();
+    accInfo.balance = await detailList[1].getText();
+    accInfo.due_date = await detailList[5].getText();
+    await fs.writeFile('accInfo.json', JSON.stringify(accInfo));
+    await driver.findElement(By.css('#logoutForm > button')).click();
+    await driver.wait(until.elementLocated(By.id('username')))
+  } finally {
+    await driver.quit();
+  }
+})();
+//
+// (async () => {
+//   let driver = await new Builder().forBrowser('chrome').build()
+//   try {
+//     await driver.get('https://www.myiclubonline.com/iclub/members/signin')
+//     await driver.
+//   } finally {
+//     await driver.quit()
+//   }
+// })()
